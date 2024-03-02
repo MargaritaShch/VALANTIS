8a9b2ffd8a0b56118956592ddbc33979a02f7db3
Rita Nguema <marvshcherbakova@gmail.com>
2024-03-02 15:44:59 +0300

    render products

--- src/script.js
+++ src/script.js
@@ -4,30 +4,39 @@ import { getProduct } from "./api/api.js"
 
 async function createProduct(){
     try {
-        const response = await getProduct("get_ids", { offset: 10, limit: 3 }, "Valantis");
+        const response = await getProduct("get_ids", { offset:3, limit: 10 }, "Valantis");
         if (response.result) {
-            const products = response.result;
-            const containerProducts = document.querySelector(".products-container");
-            products.forEach(product => {
-                const productElem = document.createElement("div");
-                productElem.classList.add("product");
-        
-                const nameProduct = document.createElement("p");
-                nameProduct.textContent = product.product;
-                productElem.appendChild(nameProduct);
-        
-                const priceProduct = document.createElement("p");
-                priceProduct.textContent = product.price;
-                productElem.appendChild(priceProduct);
-        
-                console.log(nameProduct);
-        
-                containerProducts.appendChild(productElem);
-                console.log(containerProducts)
-            });
-        } else {
-            console.error("Error: No products data received");
-        }
+            const productsId = response.result.map(product => product.id);
+            console.log("PRODUCTS ID:",  productsId)//[undefined, undefined, undefined]
+            const responseProduct = await getProduct("get_items", { ids: productsId }, "Valantis");
+            console.log("Response PRODUCTS:",  responseProduct )//Array(0)
+            if (responseProduct .result){
+                const containerProducts = document.querySelector(".products-container");
+                const elems = response.result;
+                console.log("ELEMS",elems)//['18e4e3bd-5e60-4348-8c92-4f61c676be1f', '711837ec-57f6-4145-b17f-c74c2896bafe', '6c972a4a-5b91-4a98-9780-3a19a7f41560']
+                elems.forEach(product => {
+                    const productElem = document.createElement("div");
+                    productElem.classList.add("product");
+            
+                    const nameProduct = document.createElement("p");
+                    nameProduct.textContent = product.product;
+                    productElem.appendChild(nameProduct);
+                    console.log("Name product:",nameProduct);//<p></p>
+            
+                    const priceProduct = document.createElement("p");
+                    priceProduct.textContent = product.price;
+                    productElem.appendChild(priceProduct);
+                    console.log("Price:",priceProduct);//<p></p>
+            
+                    
+            
+                    containerProducts.appendChild(productElem);
+                });
+            } else {
+                console.error("Error: No products data received");
+            }
+            }
+           
     } catch (error) {
         console.error("Error while retrieving data:", error);
     }  
